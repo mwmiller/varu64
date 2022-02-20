ef8b4f93c5a6723ab873bfa05d9759172ec40c1c
Matthew Miller <matt.miller@nfiindustries.com>
2022-02-20 09:19:31 +0200

    Prepare package for hex release.

--- mix.exs
+++ mix.exs
@@ -7,22 +7,43 @@ defmodule Varu64.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      name: "Varu64",
+      source_url: "https://github.com/mwmiller/varu64",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Varu64 - a pure Elixir variable length unsigned 64-bit integer encoding
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Matt Miller"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/mwmiller/varu64",
+        "Encoding description" => "https://github.com/AljoschaMeyer/varu64"
+      }
     ]
   end
 end
